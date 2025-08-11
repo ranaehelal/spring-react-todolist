34b89a802b53482ebc89188eaba8eb6b67f57812
rana-helal <res.maika@gmail.com>
2025-08-11 05:09:18 +0300

    add methods to TodoItemRepo and UserRepo for enhanced data retrieval

--- src/main/java/com/example/todolist/repos/TodoItemRepo.java
+++ src/main/java/com/example/todolist/repos/TodoItemRepo.java
@@ -1,11 +1,22 @@
 package com.example.todolist.repos;
 
 import com.example.todolist.model.TodoItem;
+import com.example.todolist.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+
+//has every thing i want
+// like to find all data or find by the pd (ID)
+//save new delete by ID
+
 @Repository
 public interface TodoItemRepo extends JpaRepository<TodoItem, Long> {
+    List<TodoItem> findByCompleteTrue();
+    List<TodoItem> findByCompleteFalse();
+
 
 }
 
--- src/main/java/com/example/todolist/repos/UserRepo.java
+++ src/main/java/com/example/todolist/repos/UserRepo.java
@@ -4,7 +4,11 @@ import com.example.todolist.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepo  extends JpaRepository<User, Long> {
     User findByEmail(String email);
+    Boolean existsByEmail(String email);
+
 }
